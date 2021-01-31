f8651170c6635bcc79c133ebf565ef813106f3c5
gsguglielmo <guglielmo.frati@retefrati.it>
2021-02-01 00:42:52 +0100

    Added error handling

--- src/Logger/CodingDuckLogger.php
+++ src/Logger/CodingDuckLogger.php
@@ -8,15 +8,15 @@ class CodingDuckLogger {
 
     public function __invoke(array $config): Logger {
 
-        $logger = new Logger('mainLogger');
-
-        $clientCredentials = json_decode(file_get_contents($config["credentials"]),true);
-
-        $autoSession = isset($config["autoSession"]) ? $config["autoSession"] : false;
-
-        $projectRoot = isset($config["projectRoot"]) ? $config["projectRoot"] : "";
-
         try {
+	        $logger = new Logger('mainLogger');
+
+	        $clientCredentials = json_decode(file_get_contents($config["credentials"]),true);
+
+	        $autoSession = isset($config["autoSession"]) ? $config["autoSession"] : false;
+
+	        $projectRoot = isset($config["projectRoot"]) ? $config["projectRoot"] : "";
+
             $logger->pushProcessor(new CodingDuckProcessor($clientCredentials["clientID"], $clientCredentials["clientSecret"], $projectRoot, $autoSession));
 
             $host = $config["host"];
@@ -30,7 +30,7 @@ class CodingDuckLogger {
 
 
         } catch (\Exception $e) {
-            error_log("Err ".$e->getMessage()." ".$e->getLine());
+            error_log("CodingDuck logger plugin: ".$e->getMessage()." ".$e->getLine());
         }
         return $logger;
     }
