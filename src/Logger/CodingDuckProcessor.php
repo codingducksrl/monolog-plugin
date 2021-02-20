0f4f6ae35f392121f0d076b1c49e5d5079e1e37a
gsguglielmo <guglielmo.frati@retefrati.it>
2021-02-20 21:25:53 +0100

    Added laravel support

--- src/Logger/CodingDuckProcessor.php
+++ src/Logger/CodingDuckProcessor.php
@@ -1,126 +1,139 @@
 <?php
 
 namespace Codingduck\Logger;
+
 use Monolog\Processor\ProcessorInterface;
 use Ramsey\Uuid\Uuid;
 
-class CodingDuckProcessor implements ProcessorInterface{
+class CodingDuckProcessor implements ProcessorInterface {
 
-    private string $clientID;
-    private string $clientSecret;
-    private string $projectRoot;
-    private string $transaction;
-    private $session ;
+	private string $clientID;
+	private string $clientSecret;
+	private string $projectRoot;
+	private string $transaction;
+	private $session;
 
-    public function __construct($clientID,$clientSecret,$projectRoot,$autoSession) {
-        $this->clientID = $clientID;
-        $this->clientSecret = $clientSecret;
-        $this->projectRoot = $projectRoot;
-        $this->transaction = Uuid::uuid4()->toString();
-        $this->session = null;
-        if($autoSession){
-            session_name("coding_duck_logger_session");
-            session_start();
-            if(!isset($_SESSION["sid"])){
-                $_SESSION["sid"] = \Ramsey\Uuid\Uuid::uuid4()->toString();
-            }
-            session_write_close();
-            $this->session = $_SESSION["sid"];
-        }
-    }
+	public function __construct($clientID, $clientSecret, $projectRoot, $autoSession) {
+		$this->clientID = $clientID;
+		$this->clientSecret = $clientSecret;
+		$this->projectRoot = $projectRoot;
+		$this->transaction = Uuid::uuid4()->toString();
+		$this->session = null;
+		if ($autoSession) {
+			session_name("coding_duck_logger_session");
+			session_start();
+			if (!isset($_SESSION["sid"])) {
+				$_SESSION["sid"] = \Ramsey\Uuid\Uuid::uuid4()->toString();
+			}
+			session_write_close();
+			$this->session = $_SESSION["sid"];
+		}
+	}
 
-    public function __invoke(array $record) {
+	private function strpos(string $haystack, array $needles) {
+		foreach ($needles as $needle) {
+			if (strpos($haystack, $needle)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-        $raw = $record["context"];
+	public function __invoke(array $record) {
 
-        $raw["level"] = $record["level_name"];
-        $raw["message"] = $record["message"];
-        $raw["timestamp"] = $record["datetime"];//.format(DateTimeInterface::RFC3339_EXTENDED);
-        $raw["id"] = Uuid::uuid4()->toString();
-        $raw["transaction"] = $this->transaction;
+		$raw = $record["context"];
 
-        if(isset($raw["session"])){
-            $this->session = $raw["session"];
-        }elseif ($this->session != null){
-            $raw["session"] = $this->session;
-        }
+		$raw["level"] = $record["level_name"];
+		$raw["message"] = $record["message"];
+		$raw["timestamp"] = $record["datetime"];//.format(DateTimeInterface::RFC3339_EXTENDED);
+		$raw["id"] = Uuid::uuid4()->toString();
+		$raw["transaction"] = $this->transaction;
 
-        if(isset($raw["errorObject"]) && $raw["errorObject"] instanceof \Throwable){
-            $error = $raw["errorObject"];
-            $trace = $error->getTrace();
-            $final = [];
-            try{
+		if (isset($raw["session"])) {
+			$this->session = $raw["session"];
+		} elseif ($this->session != null) {
+			$raw["session"] = $this->session;
+		}
 
-                foreach ($trace as $line){
-                    $final[] = [
-                        "filename" => str_replace($this->projectRoot,"",$line["file"]),
-                        "function" => $line["function"],
-                        "line" => $line["line"],
-                        "class" => $line["class"],
-                        "column" => 0
-                    ];
-                }
-            }catch (\Exception $e){
+		if (isset($raw["errorObject"]) && $raw["errorObject"] instanceof \Throwable) {
+			$error = $raw["errorObject"];
+			$trace = $error->getTrace();
+			$final = [];
+			try {
 
-            }
+				foreach ($trace as $line) {
+					$final[] = [
+						"filename" => str_replace($this->projectRoot, "", $line["file"]),
+						"function" => $line["function"],
+						"line" => $line["line"],
+						"class" => $line["class"],
+						"column" => 0
+					];
+				}
+			} catch (\Exception $e) {
 
-            $raw["errorObject"] = [
-                "message" => $error->getMessage(),
-                "code" => $error->getCode()
-            ];
+			}
 
-            $raw["location"] = [
-                "filename" => str_replace($this->projectRoot,"",$error->getFile()),
-                "line" => $error->getLine(),
-                "column" => 0,
-                "fullstack" => $final
-            ];
+			$raw["errorObject"] = [
+				"message" => $error->getMessage(),
+				"code" => $error->getCode()
+			];
 
-            //unset($raw["errorObject"]);
-        }else{
-            $trace = debug_backtrace();
+			$raw["location"] = [
+				"filename" => str_replace($this->projectRoot, "", $error->getFile()),
+				"line" => $error->getLine(),
+				"column" => 0,
+				"fullstack" => $final
+			];
 
-            $final = [];
+			//unset($raw["errorObject"]);
+		} else {
+			$trace = debug_backtrace();
 
-            $register = false;
+			$final = [];
 
-            foreach ($trace as $item) {
-                if($register && isset($item["file"],$item["function"],$item["line"],$item["class"])){
-                    $final[] = [
-                        "filename" => str_replace($this->projectRoot,"",$item["file"]),
-                        "function" => $item["function"],
-                        "line" => $item["line"],
-                        "class" => $item["class"],
-                        "column" => 0
-                    ];
-                }elseif (isset($item["file"]) && strpos($item["file"],"vendor/illuminate/support/Facades/Facade.php") !== false){
-                    $register = true;
-                }
+			$register = false;
+
+			foreach ($trace as $item) {
+				if ($register && isset($item["file"], $item["function"], $item["line"], $item["class"])) {
+					$final[] = [
+						"filename" => str_replace($this->projectRoot, "", $item["file"]),
+						"function" => $item["function"],
+						"line" => $item["line"],
+						"class" => $item["class"],
+						"column" => 0
+					];
+				} elseif (isset($item["file"]) && $this->strpos($item["file"], [
+						"vendor/illuminate/support/Facades/Facade.php",
+						"vendor/laravel/framework/src/Illuminate/Support/Facades/Facade.php"
+					]) !== false) {
+					$register = true;
+				}
 
 
-            }
-            if(count($final) > 0){
-                $raw["location"] = [
-                    "filename" => $final[0]["filename"],
-                    "line" => $final[0]["line"],
-                    "column" => $final[0]["column"],
-                    "fullstack" => $final
-                ];
-            }
+			}
+			if (count($final) > 0) {
+				$raw["location"] = [
+					"filename" => $final[0]["filename"],
+					"line" => $final[0]["line"],
+					"column" => $final[0]["column"],
+					"fullstack" => $final
+				];
+			}
 
-        }
+		}
 
-        return [
-            "log" => base64_encode(json_encode($raw)),
-            "authentication" => [
-                "logID" => $raw["id"],
-                "clientID" => $this->clientID,
-                "clientSecret" => hash("sha512",$raw["id"].$this->clientSecret )
-            ],
-            "level" => $record["level"],
-            "extra" => [],
-            "context" => []
-        ];
+		return [
+			"log" => base64_encode(json_encode($raw)),
+			"authentication" => [
+				"logID" => $raw["id"],
+				"clientID" => $this->clientID,
+				"clientSecret" => hash("sha512", $raw["id"] . $this->clientSecret)
+			],
+			"level" => $record["level"],
+			"extra" => [],
+			"context" => []
+		];
 
-    }
+	}
 }
